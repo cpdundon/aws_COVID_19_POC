0c35c1a9527d10624d0fb053dd35495c797aae23
Christopher P. Dundon <chris.dundon@gmail.com>
2020-04-11 00:43:39 -0400

    looking to bring back 3 sets of data with 3 fetch calls from one button push.  it took me a while to figure out how to do it and i think that i have it figured.  now i need to build the solution.  it just exists in my head

--- js/dundonCDC.js
+++ js/dundonCDC.js
@@ -7,16 +7,24 @@ function main () {
 } 
 
 const getData = () => {
-	const url = 'https://api.covid19api.com/total/country/us/status/confirmed'
+	//const url = 'https://api.covid19api.com/total/country/us/status/confirmed'
+	const url = 'https://api.covid19api.com/total/country/us/status/deaths'
+	const info = {}
 
+	fetchData (url, info, true)
+}
+
+const fetchData = (url, info, deaths) => {
+	
+	console.log('fetching: ', url)
 	fetch(url)
 		.then(data => data.text())
 		.then((text) => {
 			let json = JSON.parse(text)
 			console.log('request succeeded with JSON response')
-			// console.log(json)
 			console.log('Array length: ', json.length)
 			fillTable(json)
+			parseInfo(json, info, deaths)
 		}).catch(function (error) {
 			console.log('request failed', error)
 		});
@@ -46,10 +54,78 @@ const fillTable = (json) => {
 
 		html += "<tr>"
 		html += "<td>" + dtStr + "</td>"
-		html += "<td>" + nfObject.format(cases) + "</td>"
 		html += "<td>" + nfObject.format(cases - cases_m1) + "</td>"
+		html += "<td>" + nfObject.format(cases) + "</td>"
 		html += "</tr>"
 	}
 
 	$(html).insertAfter("tr#head-tr")
-} 
+}
+
+ 
+const parseInfo = (allJson, deathJson) => {
+	let dt = new Date(Date.UTC(1970, 0, 1))
+	let dtStr = dt.toISOString().substring(0,10)
+	allBegin = new Date(allJson[0].Date)
+	deathBegin = new Date(deathJson[0].Date)
+	firstDt = Math.min(allBegin, deathBegin)
+
+	allEnd = new Date(allJson[allJson.length-1].Date)
+	deathEnd = new Date(deathEnd[deathJson.length-1].Date)
+	lastDt = Math.max(allEnd, deathEnd)
+
+	const dayCount = (lastDt - firstDt) / (24 * 3600 * 1000)
+
+	const dayDict = {}
+	for (i=0; i<dayCount; i++) {
+		const dt = firstDt + (24 * 3600 * 1000) * i
+		dayDict[dt.toISOString().substring(0,10)] = new CovidInfo( dt ) 
+	}
+
+	let cases_m1 = 0
+	for (i=0; i<allJson.length - 1; i++) {
+		const dtStr = allJson[i].Date.substring(0,10)
+		const ci = dayDict[dtStr]
+		const cases = allJson[i].Cases
+		const diff = cases - cases_m1
+		ci.setCases(cases)
+		ci.setNewCases(diff)
+
+		cases_m1 = cases
+	}
+
+	cases_m1 = 0
+	for (i=0; i<deathJson.length - 1; i++) {
+		const dtStr = deathJson[i].Date.substring(0,10)
+		const ci = dayDict[dtStr]
+		const cases = deathJson[i].Cases
+		const diff = cases - cases_m1
+		ci.setCases(cases)
+		ci.setNewCases(diff)
+
+		cases_m1 = cases
+	}
+
+	return dayDict
+}
+
+class CovidInfo {
+	constructor(dt) {
+		this.date = dt
+		this.newCases = -1
+		this.cases = -1
+		this.newDeaths = -1
+		this.deaths = -1
+	}
+
+	get date { return this.date }
+	get newCases { return this.newCases}
+	get cases { return this.cases}
+	get newDeaths { return this.newDeaths }
+	get deaths { return this.deaths }
+
+	setNewCases (newCases) { this.newCases = newCases  }
+	setCases (cases) {this.cases = cases }
+	setNewDeaths (newDeaths) { this.newDeaths = newDeaths  }
+	setDeaths (deaths) { this.deaths = deaths  }
+}
